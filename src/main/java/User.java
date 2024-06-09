97fd7acceda941612eef6ea6864f67668b1769a0
Jony-1 <jonathangalindo1101@gmail.com>
2024-06-09 16:47:56 -0500

    desarrollo grafos y integracion del desarrollo del insert

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -1,27 +1,70 @@
 public class User {
     private String idUser;
-    private String username;
-    private String lastname;
+    private String name;
+    private String lastName;
 
-    public User(String idUser, String username, String lastname) {
+    public User(String idUser, String name, String lastName) {
         this.idUser = idUser;
-        this.username = username;
-        this.lastname = lastname;
+        this.name = name;
+        this.lastName = lastName;
     }
 
     public String getIdUser() {
         return idUser;
     }
 
-    public String getUsername() {
-        return username;
+    public String getName() {
+        return name;
     }
 
-    public String getLastname() {
-        return lastname;
+    public String getLastName() {
+        return lastName;
     }
 
-    public <T> Comparable<T> getId() {
-        return null;
+    public void setIdUser(String idUser) {
+        this.idUser = idUser;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
+public class User {
+    private String idUser;
+    private String name;
+    private String lastName;
+
+    public User(String idUser, String name, String lastName) {
+        this.idUser = idUser;
+        this.name = name;
+        this.lastName = lastName;
+    }
+
+    public String getIdUser() {
+        return idUser;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setIdUser(String idUser) {
+        this.idUser = idUser;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
     }
 }
